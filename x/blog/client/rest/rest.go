c59732947de7360508358ec1c5f9cdeb7a3407a4
Denis Fadeev <denis@fadeev.org>
2020-07-25 21:34:24 +0500

    03-http-list-posts

--- x/blog/client/rest/rest.go
+++ x/blog/client/rest/rest.go
@@ -4,10 +4,27 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
+
+	"fmt"
+	"net/http"
+
+	"github.com/cosmos/cosmos-sdk/types/rest"
 )
 
 // RegisterRoutes registers blog-related REST handlers to a router
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
-  // this line is used by starport scaffolding
+	// this line is used by starport scaffolding
 	r.HandleFunc("/faucet", faucetHandler(cliCtx)).Methods("POST")
+	r.HandleFunc("/blog/posts", listPostsHandler(cliCtx, "blog")).Methods("GET")
+}
+
+func listPostsHandler(cliCtx context.CLIContext, storeName string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/list-posts", storeName), nil)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+			return
+		}
+		rest.PostProcessResponse(w, cliCtx, res)
+	}
 }
